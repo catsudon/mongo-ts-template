37fcef0da285ba42ad11fa318383d22018761b31
Mark48853 <sathanalao@hotmail.com>
2022-09-13 14:31:00 +0700

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -28,17 +28,19 @@ const startServer = () => {
             /** Log the Response */
             Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}]`);
         });
+
+        next();
     });
 
-    router.use(express.urlencoded({ extended: true}));
+    router.use(express.urlencoded({ extended: true }));
     router.use(express.json());
 
     /** Rules of API */
     router.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-        if(req.method == 'OPTIONS') {
+        if (req.method == 'OPTIONS') {
             res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
             return res.status(200).json({});
         }
@@ -49,15 +51,16 @@ const startServer = () => {
     /** Routes */
 
     /** Healthcheck */
-    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong'}));
+    router.get('/ping', (req, res, next) => res.status(200).json({ message:'pong' }));
 
     /** Error Handling */
     router.use((req, res, next) => {
         const error = new Error('not found');
+
         Logging.error(error);
 
-        return res.status(404).json({ message: error.message});
-    })
+        return res.status(404).json({ message: error.message });
+    });
 
-    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`))
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}`));
 };
